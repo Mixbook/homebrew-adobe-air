f03c0bff2adc2030798bc0fa79cc31de92988280
Dan Schultz <schultz.t.dan@gmail.com>
2016-04-12 14:43:17 -0700

    Add Adobe AIR Compiler v21.0.0.176

---
+++ adobe-air-sdk.rb
@@ -0,0 +1,26 @@
+# Find downloads at: https://helpx.adobe.com/air/kb/archived-air-sdk-version.html
+class AdobeAirSdk < Formula
+  desc "SDK for Adobe Air"
+  homepage "https://www.adobe.com/devnet/air/air-sdk-download.html"
+  url "https://s3.amazonaws.com/air-sdks/AIRSDK_Compiler-21.0.tbz2"
+  version "21.0.0.176"
+  sha256 "26db85d4c224d084a41a195b1d6e05aad79ee807e44e9f6e13f4484837268cc7"
+
+  bottle :unneeded
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    libexec.install Dir["*"]
+    bin.write_exec_script Dir["#{libexec}/bin/*"]
+  end
+
+  def caveats; <<-EOS.undent
+    To set AIR_HOME:
+      export AIR_HOME=#{libexec}
+    EOS
+  end
+
+  test do
+    assert_equal "#{version}\n", shell_output("#{bin}/adt -version")
+  end
+end

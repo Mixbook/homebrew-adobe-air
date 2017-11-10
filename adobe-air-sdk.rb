30ecb6234411c8ba3378468e1b16d645abffa02d
Thomas Erickson <grumpy@users.noreply.github.com>
2017-11-09 17:04:46 -0800

    Update adobe-air-sdk.rb
    Seeing if using http instead of https will unblock the build.

--- adobe-air-sdk.rb
+++ adobe-air-sdk.rb
@@ -2,7 +2,7 @@
 class AdobeAirSdk < Formula
   desc "SDK for Adobe Air"
   homepage "https://www.adobe.com/devnet/air/air-sdk-download.html"
-  url "https://s3.amazonaws.com/air-sdks/AIRSDK_Compiler-21.0.tbz2"
+  url "http://s3.amazonaws.com/air-sdks/AIRSDK_Compiler-21.0.tbz2"
   version "21.0.0.176"
   sha256 "26db85d4c224d084a41a195b1d6e05aad79ee807e44e9f6e13f4484837268cc7"
 
